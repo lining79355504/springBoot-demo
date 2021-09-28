47acb0bc5391b5167916d842b9218367946df703
mort <mortaceli@gmail.com>
2021-09-28 11:39:14 +0800

    commit

---
+++ src/main/java/com/example/demo/WarApplication.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * @author mort
+ * @Description
+ * @date 2021/9/28
+ *
+ * 打war包入口实现
+ *
+ **/
+@MapperScan("com.demo.dao")
+@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
+@SpringBootApplication(scanBasePackages = {"com.example","com.service"})
+public class WarApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(WarApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarApplication.class, args);
+    }
+}
