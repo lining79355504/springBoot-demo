d4d301e1deac9b5b9f769413806844aa88b9eea9
mort <mortaceli@gmail.com>
2021-10-20 13:53:06 +0800

    fix

--- src/main/java/com/demo/config/MybatisConfig.java
+++ src/main/java/com/demo/config/MybatisConfig.java
@@ -3,7 +3,14 @@ package com.demo.config;
 import com.demo.config.mybatis.plugins.ShowSql;
 import org.apache.ibatis.session.Configuration;
 import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
 
 /**
  * @author mort
@@ -11,6 +18,7 @@ import org.springframework.context.annotation.Bean;
  * @date 2021/10/9
  **/
 @org.springframework.context.annotation.Configuration
+@ConditionalOnBean({DataSource.class})
 public class MybatisConfig {
 
     //将插件加入到mybatis插件拦截链中
@@ -24,6 +32,12 @@ public class MybatisConfig {
                 configuration.addInterceptor(myPlugin);
             }
         };
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(DataSource.class)
+    public DataSourceTransactionManager readTransactionManager(@Qualifier("ds1") DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
     }
 
 }
